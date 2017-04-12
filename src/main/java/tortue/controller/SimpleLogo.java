998dc5d0c5b96f16a52019dab07ae861bb99dfa2
NicolasDiab <nicolas-diab@hotmail.fr>
2017-04-12 11:16:49 +0200

    separate view from controller (2)

--- src/main/java/tortue/controller/SimpleLogo.java
+++ src/main/java/tortue/controller/SimpleLogo.java
@@ -11,130 +11,161 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 
 /*************************************************************************
 
-	Un petit Logo minimal qui devra etre ameliore par la suite
+ Un petit Logo minimal qui devra etre ameliore par la suite
 
-				J. Ferber - 1999-2001
+ J. Ferber - 1999-2001
 
-				Cours de DESS TNI - Montpellier II
+ Cours de DESS TNI - Montpellier II
 
-	@version 2.0
-	@date 25/09/
+ @version 2.0
+ @date 25/09/
 
 
-**************************************************************************/
+ **************************************************************************/
 
 
 public class SimpleLogo implements ActionListener {
-	private FeuilleDessin feuille;
-	private Tortue courante;
-	private Window window;
-	
-	private void quitter() {
-		System.exit(0);
-	}
+    private FeuilleDessin feuille;
+    private Tortue courante;
+    private Window window;
 
-	public SimpleLogo(Window window) {
-		this.window = window;
-	}
+    private void quitter() {
+        System.exit(0);
+    }
 
-	public String getInputValueString(){
-		return this.window.getInputValue().getText();
-	}
+    public SimpleLogo(Window window) {
+        this.setWindow(window);
+    }
 
-	@Override
-	/** la gestion des actions des boutons */
-	public void actionPerformed(ActionEvent e)
-	{
-		String c = e.getActionCommand();
+    public String getInputValueString() {
+        return this.getWindow().getInputValue().getText();
+    }
 
-		switch (c) {
-			case "Avancer":
-				System.out.println("command avancer");
-				try {
-					int v = Integer.parseInt(this.getInputValueString());
-					courante.avancer(v);
-				} catch (NumberFormatException ex){
-					System.err.println("ce n'est pas un nombre : " + this.getInputValueString());
-				}
-				break;
-			case "Droite":
-				try {
-					int v = Integer.parseInt(this.getInputValueString());
-					courante.droite(v);
-				} catch (NumberFormatException ex){
-					System.err.println("ce n'est pas un nombre : " + this.getInputValueString());
-				}
-				break;
-			case "Gauche":
-				try {
-					int v = Integer.parseInt(this.getInputValueString());
-					courante.gauche(v);
-				} catch (NumberFormatException ex){
-					System.err.println("ce n'est pas un nombre : " + this.getInputValueString());
-				}
-				break;
-			case "Lever":
-				courante.leverCrayon();
-				break;
-			case "Baisser":
-				courante.baisserCrayon();
-				// actions des boutons du bas
-			case "Proc1":
-				proc1();
-				break;
-			case "Proc2":
-				proc2();
-				break;
-			case "Proc3":
-				proc3();
-				break;
-			case "Effacer":
-				effacer();
-				break;
-			case "Quitter":
-				quitter();
-				break;
-		}
+    @Override
+    /** la gestion des actions des boutons */
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() instanceof JComboBox) {
+            JComboBox cb = (JComboBox) e.getSource();
+            int n = cb.getSelectedIndex();
+            getCourante().setColor(n);
+        } else {
+            String c = e.getActionCommand();
 
-		window.repaint();
-	}
+            switch (c) {
+                case "Avancer":
+                    System.out.println("command avancer");
+                    try {
+                        int v = Integer.parseInt(this.getInputValueString());
+                        getCourante().avancer(v);
+                    } catch (NumberFormatException ex) {
+                        System.err.println("ce n'est pas un nombre : " + this.getInputValueString());
+                    }
+                    break;
+                case "Droite":
+                    try {
+                        int v = Integer.parseInt(this.getInputValueString());
+                        getCourante().droite(v);
+                    } catch (NumberFormatException ex) {
+                        System.err.println("ce n'est pas un nombre : " + this.getInputValueString());
+                    }
+                    break;
+                case "Gauche":
+                    try {
+                        int v = Integer.parseInt(this.getInputValueString());
+                        getCourante().gauche(v);
+                    } catch (NumberFormatException ex) {
+                        System.err.println("ce n'est pas un nombre : " + this.getInputValueString());
+                    }
+                    break;
+                case "Lever":
+                    getCourante().leverCrayon();
+                    break;
+                case "Baisser":
+                    getCourante().baisserCrayon();
+                    // actions des boutons du bas
+                case "Proc1":
+                    proc1();
+                    break;
+                case "Proc2":
+                    proc2();
+                    break;
+                case "Proc3":
+                    proc3();
+                    break;
+                case "Effacer":
+                    effacer();
+                    break;
+                case "Quitter":
+                    quitter();
+                    break;
+            }
 
-	public void manageAction(String c){
-		// actions des boutons du haut
+            getWindow().repaint();
+        }
+    }
+
+    public void manageAction(String c) {
+        // actions des boutons du haut
 
 
-		feuille.repaint();
-	}
+        getFeuille().repaint();
+    }
 
-  	/** les procedures Logo qui combinent plusieurs commandes..*/
+    /**
+     * les procedures Logo qui combinent plusieurs commandes..
+     */
 
   	/* Square */
-	public void proc1() {
-		new Square(courante);
-	}
-  	/* Poly */
-	public void proc2() {
-		new Poly(courante, 60, 8);
-	}
-  	/* Spiral */
-	public void proc3() {
-		new Spiral(courante, 50,40,6);
-	}
+    public void proc1() {
+        new Square(getCourante());
+    }
 
-	// efface tout et reinitialise la feuille
-	public void effacer() {
-		feuille.reset();
-		feuille.repaint();
+    /* Poly */
+    public void proc2() {
+        new Poly(getCourante(), 60, 8);
+    }
 
-		// Replace la tortue au centre
-		Dimension size = feuille.getSize();
-		courante.setPosition(size.width/2, size.height/2);
-	}
+    /* Spiral */
+    public void proc3() {
+        new Spiral(getCourante(), 50, 40, 6);
+    }
+
+    // efface tout et reinitialise la feuille
+    public void effacer() {
+        getFeuille().reset();
+        getFeuille().repaint();
+
+        // Replace la tortue au centre
+        Dimension size = getFeuille().getSize();
+        getCourante().setPosition(size.width / 2, size.height / 2);
+    }
+
+
+    public FeuilleDessin getFeuille() {
+        return feuille;
+    }
+
+    public void setFeuille(FeuilleDessin feuille) {
+        this.feuille = feuille;
+    }
+
+    public Tortue getCourante() {
+        return courante;
+    }
+
+    public void setCourante(Tortue courante) {
+        this.courante = courante;
+    }
+
+    public Window getWindow() {
+        return window;
+    }
+
+    public void setWindow(Window window) {
+        this.window = window;
+    }
 }
--- src/main/java/tortue/view/Window.java
+++ src/main/java/tortue/view/Window.java
@@ -9,8 +9,8 @@ import java.awt.event.*;
 
 public class Window extends JFrame {
 
-    private static final Dimension VGAP = new Dimension(1,5);
-    private static final Dimension HGAP = new Dimension(5,1);
+    private static final Dimension VGAP = new Dimension(1, 5);
+    private static final Dimension HGAP = new Dimension(5, 1);
     private SimpleLogo controller;
 
     private JTextField inputValue;
@@ -19,15 +19,15 @@ public class Window extends JFrame {
      * @param args
      */
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable(){
-            public void run(){
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
                 Window window = new Window();
             }
         });
 
     }
 
-    public Window(){
+    public Window() {
 
         super("un logo tout simple");
         this.setVisible(true);
@@ -68,7 +68,7 @@ public class Window extends JFrame {
         m.add(menuItem);
 
         menuItem.setActionCommand(command);
-        menuItem.addActionListener(this);
+        menuItem.addActionListener(this.getController());
         if (key > 0) {
             if (key != KeyEvent.VK_DELETE)
                 menuItem.setAccelerator(KeyStroke.getKeyStroke(key, Event.CTRL_MASK, false));
@@ -81,39 +81,37 @@ public class Window extends JFrame {
     public void addButton(JComponent p, String name, String tooltiptext, String imageName) {
         JButton b;
         if ((imageName == null) || (imageName.equals(""))) {
-            b = (JButton)p.add(new JButton(name));
-        }
-        else {
+            b = (JButton) p.add(new JButton(name));
+        } else {
             java.net.URL u = this.getClass().getResource(imageName);
             if (u != null) {
-                ImageIcon im = new ImageIcon (u);
+                ImageIcon im = new ImageIcon(u);
                 b = (JButton) p.add(new JButton(im));
-            }
-            else
+            } else
                 b = (JButton) p.add(new JButton(name));
             b.setActionCommand(name);
         }
 
         b.setToolTipText(tooltiptext);
         b.setBorder(BorderFactory.createRaisedBevelBorder());
-        b.setMargin(new Insets(0,0,0,0));
-        b.addActionListener(this);
+        b.setMargin(new Insets(0, 0, 0, 0));
+        b.addActionListener(this.getController());
     }
 
     public void logoInit() {
-        getContentPane().setLayout(new BorderLayout(10,10));
+        getContentPane().setLayout(new BorderLayout(10, 10));
 
         // Boutons
         JToolBar toolBar = new JToolBar();
         JPanel buttonPanel = new JPanel();
         buttonPanel.add(toolBar);
 
-        getContentPane().add(buttonPanel,"North");
+        getContentPane().add(buttonPanel, "North");
 
-        addButton(toolBar,"Effacer","Nouveau dessin","/icons/index.png");
+        addButton(toolBar, "Effacer", "Nouveau dessin", "/icons/index.png");
 
         toolBar.add(Box.createRigidArea(getHGAP()));
-        setInputValue(new JTextField("45",5));
+        setInputValue(new JTextField("45", 5));
         toolBar.add(getInputValue());
         addButton(toolBar, "Avancer", "Avancer 50", null);
         addButton(toolBar, "Droite", "Droite 45", null);
@@ -121,7 +119,7 @@ public class Window extends JFrame {
         addButton(toolBar, "Lever", "Lever Crayon", null);
         addButton(toolBar, "Baisser", "Baisser Crayon", null);
 
-        String[] colorStrings = {"noir", "bleu", "cyan","gris fonce","rouge",
+        String[] colorStrings = {"noir", "bleu", "cyan", "gris fonce", "rouge",
                 "vert", "gris clair", "magenta", "orange",
                 "gris", "rose", "jaune"};
 
@@ -132,25 +130,18 @@ public class Window extends JFrame {
         JComboBox colorList = new JComboBox(colorStrings);
         toolBar.add(colorList);
 
-        colorList.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JComboBox cb = (JComboBox)e.getSource();
-                int n = cb.getSelectedIndex();
-                courante.setColor(n);
-            }
-        });
-
+        colorList.addActionListener(this.getController());
 
         // Menus
-        JMenuBar menubar=new JMenuBar();
-        setJMenuBar(menubar);	// on installe le menu bar
-        JMenu menuFile=new JMenu("File"); // on installe le premier menu
+        JMenuBar menubar = new JMenuBar();
+        setJMenuBar(menubar);    // on installe le menu bar
+        JMenu menuFile = new JMenu("File"); // on installe le premier menu
         menubar.add(menuFile);
 
         addMenuItem(menuFile, "Effacer", "Effacer", KeyEvent.VK_N);
         addMenuItem(menuFile, "Quitter", "Quitter", KeyEvent.VK_Q);
 
-        JMenu menuCommandes=new JMenu("Commandes"); // on installe le premier menu
+        JMenu menuCommandes = new JMenu("Commandes"); // on installe le premier menu
         menubar.add(menuCommandes);
         addMenuItem(menuCommandes, "Avancer", "Avancer", -1);
         addMenuItem(menuCommandes, "Droite", "Droite", -1);
@@ -158,7 +149,7 @@ public class Window extends JFrame {
         addMenuItem(menuCommandes, "Lever Crayon", "Lever", -1);
         addMenuItem(menuCommandes, "Baisser Crayon", "Baisser", -1);
 
-        JMenu menuHelp=new JMenu("Aide"); // on installe le premier menu
+        JMenu menuHelp = new JMenu("Aide"); // on installe le premier menu
         menubar.add(menuHelp);
         addMenuItem(menuHelp, "Aide", "Help", -1);
         addMenuItem(menuHelp, "A propos", "About", -1);
@@ -169,31 +160,33 @@ public class Window extends JFrame {
         JPanel p2 = new JPanel(new GridLayout());
         JButton b20 = new JButton("Proc1");
         p2.add(b20);
-        b20.addActionListener(this);
+        b20.addActionListener(this.getController());
         JButton b21 = new JButton("Proc2");
         p2.add(b21);
-        b21.addActionListener(this);
+        b21.addActionListener(this.getController());
         JButton b22 = new JButton("Proc3");
         p2.add(b22);
-        b22.addActionListener(this);
+        b22.addActionListener(this.getController());
 
-        getContentPane().add(p2,"South");
+        getContentPane().add(p2, "South");
 
-        feuille = new FeuilleDessin(); //500, 400);
+        // feuille
+        FeuilleDessin feuille = new FeuilleDessin(); //500, 400);
         feuille.setBackground(Color.white);
-        feuille.setSize(new Dimension(600,400));
-        feuille.setPreferredSize(new Dimension(600,400));
-
-        getContentPane().add(feuille,"Center");
+        feuille.setSize(new Dimension(600, 400));
+        feuille.setPreferredSize(new Dimension(600, 400));
 
         // Creation de la tortue
         Tortue tortue = new Tortue();
 
         // Deplacement de la tortue au centre de la feuille
-        tortue.setPosition(500/2, 400/2);
+        tortue.setPosition(500 / 2, 400 / 2);
 
-        courante = tortue;
+        this.getController().setCourante(tortue);
         feuille.addTortue(tortue);
+
+        this.getController().setFeuille(feuille);
+        getContentPane().add(this.controller.getFeuille(), "Center");
 
         pack();
         setVisible(true);
