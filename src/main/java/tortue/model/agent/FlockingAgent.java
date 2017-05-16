9d6f75b6e27a07eb0ebdd37bfc25a0878fc23495
GregoirePiat <gregoire.piat@outlook.fr>
2017-05-16 15:27:02 +0200

    Change direction of two elements at the same position

--- src/main/java/tortue/model/agent/FlockingAgent.java
+++ src/main/java/tortue/model/agent/FlockingAgent.java
@@ -9,7 +9,7 @@ import java.util.Random;
 
 public class FlockingAgent extends BaseAgent {
 
-    private final int RANGE = 50;
+    private final int RANGE = 20;
     private final int MAX_DIR_CHANGE = 15;
     private final int TURTLE_SPEED = 5;
     private final int TURTLE_LOW_SPEED = 3;
@@ -27,11 +27,11 @@ public class FlockingAgent extends BaseAgent {
             int nbTurtles = this.getTurtles().size();
 
             int averageDir = 0;
+            Random rand = new Random();
 
             for (Tortue turtle : this.getTurtles()) {
                 // neighbours turtles
                 List<Tortue> flock = this.getEnvironnement().getTurtlesInRange(turtle.getX(), turtle.getY(), this.RANGE);
-
 
                 /**
                  * Compute average dir
@@ -39,6 +39,22 @@ public class FlockingAgent extends BaseAgent {
                 averageDir = (((int) flock.stream()
                         .mapToInt(t -> t.getDir())
                         .average().getAsDouble()));
+
+
+                //for (Tortue item : flock){
+//
+                //    int minRangeX = item.getX() - 4;
+                //    int maxRangeX = item.getX() + 4;
+                //    int minRangeY = item.getY() - 4;
+                //    int maxRangeY = item.getY() + 4;
+//
+                //    if (turtle.getX() > minRangeX && turtle.getX() < maxRangeX)
+                //        turtle.setDir(averageDir + rand.nextInt(5));
+//
+                //    if (turtle.getY() > minRangeY && turtle.getY() < maxRangeY)
+                //        turtle.setDir(averageDir + rand.nextInt(5));
+                //}
+
 
                 /**
                  * Set a maximum of dir change to 30 degrees
